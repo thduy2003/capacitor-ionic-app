51f39cbd881d901423fd3360d48cca476d96c93c
thduy2003 <duytrieudong@gmail.com>
2025-03-10 16:50:13 +0700

    navigating with nav controller + toolbar buttons

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../../place.model';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -7,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class OfferBookingsPage implements OnInit {
-
-  constructor() { }
+  place: Place | undefined;
+  
+  constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId') as string)
+    })
   }
 
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -32,4 +32,8 @@ export class PlacesService {
     return [...this._places];
   }
   constructor() {}
+  
+  getPlace(id: string) {
+    return this._places.find(p => p.id === id);
+  }
 }
